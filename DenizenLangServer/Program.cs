b8bf1b6be858ba0c975671f632b761700d25d504
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-05-27 10:33:18 -0700

    update langserver to netcore3.1 and C# 8

--- DenizenLangServer/Program.cs
+++ DenizenLangServer/Program.cs
@@ -16,47 +16,45 @@ namespace DenizenLangServer
     {
         static void Main(string[] args)
         {
-            using (Stream cin = Console.OpenStandardInput())
-            using (BufferedStream bcin = new BufferedStream(cin))
-            using (Stream cout = Console.OpenStandardOutput())
-            using (PartwiseStreamMessageReader reader = new PartwiseStreamMessageReader(bcin))
-            using (PartwiseStreamMessageWriter writer = new PartwiseStreamMessageWriter(cout))
+            using Stream cin = Console.OpenStandardInput();
+            using BufferedStream bcin = new BufferedStream(cin);
+            using Stream cout = Console.OpenStandardOutput();
+            using PartwiseStreamMessageReader reader = new PartwiseStreamMessageReader(bcin);
+            using PartwiseStreamMessageWriter writer = new PartwiseStreamMessageWriter(cout);
+            JsonRpcContractResolver contractResolver = new JsonRpcContractResolver
             {
-                JsonRpcContractResolver contractResolver = new JsonRpcContractResolver
-                {
-                    NamingStrategy = new CamelCaseJsonRpcNamingStrategy(),
-                    ParameterValueConverter = new CamelCaseJsonValueConverter(),
-                };
-                StreamRpcClientHandler clientHandler = new StreamRpcClientHandler();
-                JsonRpcClient client = new JsonRpcClient(clientHandler);
-                clientHandler.MessageSending += (_, e) =>
-                {
-                    Console.Error.WriteLine("Sending: " + e.Message);
-                };
-                clientHandler.MessageReceiving += (_, e) =>
-                {
-                    Console.Error.WriteLine("Receiving: " + e.Message);
-                };
-                LanguageServerSession session = new LanguageServerSession(client, contractResolver);
-                JsonRpcServiceHostBuilder builder = new JsonRpcServiceHostBuilder { ContractResolver = contractResolver };
-                builder.UseCancellationHandling();
-                builder.Register(typeof(Program).GetTypeInfo().Assembly);
-                builder.Intercept(async (context, next) =>
-                {
-                    Console.Error.WriteLine("Request: " + context.Request);
-                    await next();
-                    Console.Error.WriteLine("Response: " + context.Response);
-                });
-                IJsonRpcServiceHost host = builder.Build();
-                StreamRpcServerHandler serverHandler = new StreamRpcServerHandler(host,
-                    StreamRpcServerHandlerOptions.ConsistentResponseSequence | StreamRpcServerHandlerOptions.SupportsRequestCancellation);
-                serverHandler.DefaultFeatures.Set(session);
-                using (serverHandler.Attach(reader, writer))
-                using (clientHandler.Attach(reader, writer))
-                {
-                    // Wait for the "stop" request.
-                    session.CancellationToken.WaitHandle.WaitOne();
-                }
+                NamingStrategy = new CamelCaseJsonRpcNamingStrategy(),
+                ParameterValueConverter = new CamelCaseJsonValueConverter(),
+            };
+            StreamRpcClientHandler clientHandler = new StreamRpcClientHandler();
+            JsonRpcClient client = new JsonRpcClient(clientHandler);
+            clientHandler.MessageSending += (_, e) =>
+            {
+                Console.Error.WriteLine("Sending: " + e.Message);
+            };
+            clientHandler.MessageReceiving += (_, e) =>
+            {
+                Console.Error.WriteLine("Receiving: " + e.Message);
+            };
+            LanguageServerSession session = new LanguageServerSession(client, contractResolver);
+            JsonRpcServiceHostBuilder builder = new JsonRpcServiceHostBuilder { ContractResolver = contractResolver };
+            builder.UseCancellationHandling();
+            builder.Register(typeof(Program).GetTypeInfo().Assembly);
+            builder.Intercept(async (context, next) =>
+            {
+                Console.Error.WriteLine("Request: " + context.Request);
+                await next();
+                Console.Error.WriteLine("Response: " + context.Response);
+            });
+            IJsonRpcServiceHost host = builder.Build();
+            StreamRpcServerHandler serverHandler = new StreamRpcServerHandler(host,
+                StreamRpcServerHandlerOptions.ConsistentResponseSequence | StreamRpcServerHandlerOptions.SupportsRequestCancellation);
+            serverHandler.DefaultFeatures.Set(session);
+            using (serverHandler.Attach(reader, writer))
+            using (clientHandler.Attach(reader, writer))
+            {
+                // Wait for the "stop" request.
+                session.CancellationToken.WaitHandle.WaitOne();
             }
         }
     }
