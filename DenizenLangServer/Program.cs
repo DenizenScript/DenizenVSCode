a0f999ad7876b01433b42872575b484736c1199e
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-06-06 00:23:52 -0700

    meta doc caching for deborking reasons

--- DenizenLangServer/Program.cs
+++ DenizenLangServer/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading;
+using System.Net.Http;
+using FreneticUtilities.FreneticToolkit;
 using JsonRpc.Client;
 using JsonRpc.Contracts;
 using JsonRpc.Server;
@@ -11,6 +14,7 @@ using JsonRpc.Streams;
 using LanguageServer.VsCode;
 using SharpDenizenTools.MetaHandlers;
 using SharpDenizenTools.ScriptAnalysis;
+using System.Threading.Tasks;
 
 namespace DenizenLangServer
 {
@@ -22,12 +26,63 @@ namespace DenizenLangServer
             InitExtensionServer();
         }
 
+        public static AsciiMatcher URL_ACCEPTED = new AsciiMatcher(c => (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'));
+
         static void InitMetaHelper()
         {
             ScriptChecker.LogInternalMessage = Console.Error.WriteLine;
             MetaDocs.CurrentMeta = new MetaDocs();
-            // TODO: CACHING!
+            string cachePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            bool needsRedownload = false;
+            bool shouldIgnoreCache = false;
+            if (Directory.Exists(cachePath)) // Safety with weird OS's.
+            {
+                string cacheFolder = cachePath + "/DenizenVSCodeExtension/cache/";
+                Directory.CreateDirectory(cacheFolder);
+                MetaDocs.AlternateZipSourcer = url =>
+                {
+                    char[] urlChars = url.ToCharArray();
+                    for (int i = 0; i < urlChars.Length; i++)
+                    {
+                        if (!URL_ACCEPTED.IsMatch(urlChars[i]))
+                        {
+                            urlChars[i] = '_';
+                        }
+                    }
+                    string cachableName = new string(urlChars);
+                    string cacheFileName = cacheFolder + cachableName + ".zip";
+                    if (!shouldIgnoreCache)
+                    {
+                        if (File.Exists(cacheFileName))
+                        {
+                            if (Math.Abs(DateTime.UtcNow.Subtract(File.GetLastWriteTimeUtc(cacheFileName)).TotalHours) > 12)
+                            {
+                                needsRedownload = true;
+                            }
+                            return File.ReadAllBytes(cacheFileName);
+                        }
+                    }
+                    HttpClient client = new HttpClient
+                    {
+                        Timeout = new TimeSpan(0, 2, 0)
+                    };
+                    byte[] output = client.GetByteArrayAsync(url).Result;
+                    File.WriteAllBytes(cacheFileName, output);
+                    return output;
+                };
+            }
+            Console.Error.WriteLine();
             MetaDocs.CurrentMeta.DownloadAll();
+            if (needsRedownload)
+            {
+                shouldIgnoreCache = true;
+                Task.Factory.StartNew(() =>
+                {
+                    MetaDocs newDocs = new MetaDocs();
+                    newDocs.DownloadAll();
+                    MetaDocs.CurrentMeta = newDocs;
+                });
+            }
         }
 
         static void InitExtensionServer()
