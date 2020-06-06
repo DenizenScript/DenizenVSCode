16726d6349cf70a8dce5ae7b821ea44356216527
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-06-06 03:23:07 -0700

    don't show event matcher fails as error

--- DenizenLangServer/DiagnosticProvider.cs
+++ DenizenLangServer/DiagnosticProvider.cs
@@ -88,7 +88,14 @@ namespace DenizenLangServer
             }
             foreach (ScriptChecker.ScriptWarning warning in checker.Warnings)
             {
-                diag.Add(new Diagnostic(DiagnosticSeverity.Error, GetRange(warning), "Denizen Script Checker", "(Likely Error) " + warning.CustomMessageForm));
+                if (warning.WarningUniqueKey == "event_missing")
+                {
+                    diag.Add(new Diagnostic(DiagnosticSeverity.Hint, GetRange(warning), "Denizen Script Checker", "(Imperfect!!! Event Checking. Likely ignorable) " + warning.CustomMessageForm));
+                }
+                else
+                {
+                    diag.Add(new Diagnostic(DiagnosticSeverity.Error, GetRange(warning), "Denizen Script Checker", "(Likely Error) " + warning.CustomMessageForm));
+                }
             }
             foreach (ScriptChecker.ScriptWarning warning in checker.MinorWarnings)
             {
